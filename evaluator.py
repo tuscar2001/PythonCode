9766b6fc8dc6b364fa81931c15f37fe8c1ffb47f
tuscar2001 <tuscar2001@gmail.com>
2019-03-18 10:50:25 -0500

    Add files via upload

---
+++ evaluator.py
@@ -0,0 +1,56 @@
+def is_comment(item):
+    return isinstance(item, str) and item.startswith("#")
+
+
+def execute(program):
+    """Execute a stack program.
+    Args: Any stack like program containing where each item in the stack
+    is a callable oprators or non callabel operands. The top most items
+    on the stack may be strings beginning with # for the purposes of
+    documentation. Stack like means support for:
+    item=stack.poop(): remove and return the top item
+    stack.append(item) puch an item to the top
+    if stack   False in a boolean context when empty"""
+
+    while program:
+        item = program.pop()
+        if not is_comment(item):
+            program.append(item)
+            break
+    else:
+        print ("Empty prograrm")
+        return
+
+    #evaluate the program
+    pending = []
+
+    while program:
+        item = program.pop()
+        if callable(item):
+            try:
+                result = item(*pending)
+            except Exception as e:
+                print ("Error: ", e)
+                break
+            program.append(result)
+            pending.clear()
+        else:
+            pending.append(item)
+    else:
+        print ("Program successful. ")
+        print ("Result: ", pending)
+
+    print ("Finished")
+
+if __name__ == '__main__':
+    import operator
+
+    program = list(reversed((
+        "# A short stack program to add",
+        "# and multiply some constants",
+        5,
+        2,
+        operator.add,
+        3,
+        operator.mul)))
+    execute(program)
