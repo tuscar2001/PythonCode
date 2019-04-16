dd94b0300d3a7d3c9c7df90fdbe329d41325b055
tuscar2001 <tuscar2001@gmail.com>
2019-04-16 12:54:31 -0500

    Added date manipulation

---
+++ Dates Manipulation.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+df = pd.DataFrame({'DOB': {0: '26/1/2016', 1: '26/1/2016'}})
+print (df)
+
+
+df['DOB'] = pd.to_datetime(df.DOB)
+print (df)
+         DOB
+0 2016-01-26
+1 2016-01-26
+
+df['DOB1'] = df['DOB'].dt.strftime('%m/%d/%Y')
+print (df)
+         DOB        DOB1
+0 2016-01-26  01/26/2016
+1 2016-01-26  01/26/2016
