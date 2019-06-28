392d999e2fbe0f49365f9fa4f568b7d3ef4a24c5
tuscar2001 <tuscar2001@gmail.com>
2019-06-28 10:31:05 -0700

    Update and rename functions to functions.py

--- functions.py
+++ functions.py
@@ -1,4 +1,9 @@
 #Rolling dates
+Method1
 the data level is at key,datetime. I have to go back one level up in the group by
 test['datediff'] = test['datetime'] - test.groupby(['key'])['datetime'].transform(lambda x: x.iloc[0])
 test['datediff']=test['datediff'] / np.timedelta64(1,'D')
+
+Method2
+test['dur'] = test.groupby(['key']).datetime.apply(lambda x: x - x.iloc[0])
+test['dur']=test['dur'] / np.timedelta64(1,'D')
